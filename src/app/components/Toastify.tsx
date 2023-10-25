e106a9ebe94545700ede6165265843a5677b6233
Smooth Criminal <rafi.ogchy@gmail.com>
2023-10-25 14:54:12 +0600

    update: toastify functionality

--- src/app/components/Toastify.tsx
+++ src/app/components/Toastify.tsx
@@ -3,16 +3,6 @@ import "react-toastify/dist/ReactToastify.css";
 
 type ToastType = "success" | "warning" | "error" | "info" | "default";
 
-const actions = {
-  success: (title: string, options: ToastOptions) =>
-    toast.success(title, options),
-  warning: (title: string, options: ToastOptions) =>
-    toast.warning(title, options),
-  error: (title: string, options: ToastOptions) => toast.error(title, options),
-  info: (title: string, options: ToastOptions) => toast.info(title, options),
-  default: (title: string, options: ToastOptions) => toast(title, options),
-};
-
 const Toastify = {
   fire: (title: string, type: ToastType, options?: ToastOptions) => {
     const {
@@ -21,7 +11,8 @@ const Toastify = {
       hideProgressBar = true,
     } = options || {};
 
-    const notify = actions[type];
+    // const notify = actions[type];
+    const notify = type === "default" ? toast : toast[type];
     notify(title, { position, autoClose, hideProgressBar });
   },
 };
